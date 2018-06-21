9d7dba267ff72b979ed6693269f078ad055beac1
rohinmanvi <37752545+rohinmanvi@users.noreply.github.com>
2018-06-21 11:23:09 -0700

    Group E-mail for SIMCards Renew

--- src/com/waternotifier/waternotifierlibrary/SendHTMLEmail.java
+++ src/com/waternotifier/waternotifierlibrary/SendHTMLEmail.java
@@ -1,11 +1,9 @@
 package com.waternotifier.waternotifierlibrary;
+
+import java.util.ArrayList;
 import java.util.Properties;
 
-import javax.mail.Message;
-import javax.mail.MessagingException;
-import javax.mail.PasswordAuthentication;
-import javax.mail.Session;
-import javax.mail.Transport;
+import javax.mail.*;
 import javax.mail.internet.InternetAddress;
 import javax.mail.internet.MimeMessage;
 
@@ -16,11 +14,37 @@ public class SendHTMLEmail {
         final String username = "indiawaternotifier@gmail.com";
         final String password = "Dont4get";
 
+
+        // Recipient's email ID needs to be mentioned.
+//        String to = "rohin.manvi@gmail.com";
+        String toEmailList = "Rajesh Manvi <rimgdg@gmail.com>, Water Notifier - INDIA <indiawaternotifier@gmail.com>";
+        String ccEmailList = "Rohin Manvi <rohin.manvi@gmail.com>, Rajesh Manvi <rimgdg@gmail.com>, Rajesh Manvi <manvir@hotmail.com>";
+
+        // Sender's email ID needs to be mentioned
+        String from = "Water Notifier - INDIA <indiawaternotifier@gmail.com>";
+
+        // Assuming you are sending email from localhost or smtp.gmail.com
+        String host = "smtp.gmail.com";
+
+        // Assuming you are sending email from smtp.gmail.com and its port is 587
+//        String port = "587";
+        // For SSL
+        String port = "465";
+
+
+
         Properties props = new Properties();
         props.put("mail.smtp.auth", "true");
         props.put("mail.smtp.starttls.enable", "true");
-        props.put("mail.smtp.host", "smtp.gmail.com");
-        props.put("mail.smtp.port", "587");
+//        props.put("mail.smtp.host", "smtp.gmail.com");
+//        props.put("mail.smtp.port", "587");
+
+        props.put("mail.smtp.socketFactory.port", port);
+        props.put("mail.smtp.socketFactory.class",
+                "javax.net.ssl.SSLSocketFactory");
+
+        props.put("mail.smtp.host", host);
+        props.put("mail.smtp.port", port);
 
         Session session = Session.getInstance(props,
                 new javax.mail.Authenticator() {
@@ -30,15 +54,32 @@ public class SendHTMLEmail {
                 });
 
         try {
-
+            // Create a default MimeMessage object.
             Message message = new MimeMessage(session);
-            message.setFrom(new InternetAddress("indiawaternotifier@gmail.com"));
-            message.setRecipients(Message.RecipientType.TO,
-                    InternetAddress.parse("rimgdg@gmail.com"));
-            message.setSubject("RENEW SIM Cards - Notice 1 - within one week");
-            message.setText("Dear India WN Administrator,"
-                    + "\n\n Please renew SIM Cards, by making payments online/nearest dealer within a week !");
 
+            // Set From: header field of the header.
+//            message.setFrom(new InternetAddress("indiawaternotifier@gmail.com"));
+            message.setFrom(new InternetAddress(from));
+
+
+            // Set To: header field of the header.
+//            message.setRecipients(Message.RecipientType.TO,
+//                    InternetAddress.parse("rimgdg@gmail.com"));
+            message.setRecipients(Message.RecipientType.TO,
+                    InternetAddress.parse(toEmailList));
+
+            //            message.addRecipient(Message.RecipientType.CC, "rohin.manvi@gmail.com");
+            message.addRecipients(Message.RecipientType.CC, InternetAddress.parse(ccEmailList));
+
+            // Set Subject: header field
+            message.setSubject("RENEW SIM Cards - Notice 1 - within one week - SSL Based Message");
+
+            // Send the actual HTML message, as big as you like
+            message.setContent("<h1>This is actual message</h1>"
+                    + "<p>Dear India WN Administrator,</p>"
+                    + "<p> Please renew SIM Cards, by making payment online/with nearest dealer only a week left!</p>", "text/html");
+
+            // Send message
             Transport.send(message);
 
             System.out.println("Done");
