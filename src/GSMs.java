8cb3bcdce978ea558ee167b45f4cb823a3410925
rohinmanvi <37752545+rohinmanvi@users.noreply.github.com>
2018-07-31 17:53:17 -0700

    GSM from RSRM Game Desktop

--- src/GSMs.java
+++ src/GSMs.java
@@ -19,7 +19,7 @@ public abstract class GSMs extends GSM implements Runnable {
         threadName = name;
         function = fun;
         threadName += " (" + phone + ")";
-        LogToFile.log("info",threadName + ": creating");
+        LogToFile.log("info", threadName + ": creating");
         COMStart(comports);
         threadName += " (" + COMport + ")";
         if (t == null) {
@@ -34,13 +34,13 @@ public abstract class GSMs extends GSM implements Runnable {
         threadName = name;
         function = fun;
         threadName += " (" + phone + ")";
-        LogToFile.log("info",threadName + ": creating");
+        LogToFile.log("info", threadName + ": creating");
         boolean v = false;
         boolean reset = false;
         while (!v) {
             for (int i = 0; i < COMports.size(); i++) {
                 String port = COMports.get(i);
-                LogToFile.log("info",threadName + ": " + port);
+                LogToFile.log("info", threadName + ": " + port);
                 setPortDescription(port);
                 try {
                     if (openConnection()) {
@@ -57,12 +57,12 @@ public abstract class GSMs extends GSM implements Runnable {
                                     }
                                 } catch (Exception e) {
                                     e.printStackTrace();
-                                    LogToFile.log(e,"info", "GSMs || ");
-                                    LogToFile.log("info","CONTINUING");
+                                    LogToFile.log(e, "info", "GSMs || ");
+                                    LogToFile.log("info", "CONTINUING");
                                 }
                                 break;
                             } else {
-                                LogToFile.log("info",threadName + ": " + port + " wrong number");
+                                LogToFile.log("info", threadName + ": " + port + " wrong number");
                                 completeReset();
                             }
                         }
@@ -70,8 +70,8 @@ public abstract class GSMs extends GSM implements Runnable {
                 } catch (Exception e) {
                     closeConnection();
                     e.printStackTrace();
-                    LogToFile.log(e,"info", "GSMs || ");
-                    LogToFile.log("info","CONTINUING");
+                    LogToFile.log(e, "info", "GSMs || ");
+                    LogToFile.log("info", "CONTINUING");
                     v = false;
                 }
                 closeConnection();
@@ -95,40 +95,37 @@ public abstract class GSMs extends GSM implements Runnable {
                 count = 0;
             for (int i = 0; i <= comports; i++) {
                 String port = "COM" + i;
-                LogToFile.log("info",threadName + ": " + port);
+                LogToFile.log("info", threadName + ": " + port);
                 setPortDescription(port);
                 try {
-                    if (openConnection()) {
-                        completeReset();
-                        if (checkConnection()) {
-                            COMports.add(0, port);
-                            if (ownPhoneNumber()) {
-                                try {
-                                    if (COMports.size() > 0) {
-                                        COMports.remove(0);
-                                    }
-                                } catch (Exception e) {
-                                    e.printStackTrace();
-                                    LogToFile.log("info","CONTINUING");
+                    if (openConnection() && completeReset() && checkConnection()) {
+                        COMports.add(0, port);
+                        if (ownPhoneNumber()) {
+                            try {
+                                if (COMports.size() > 0) {
+                                    COMports.remove(0);
                                 }
-                                v = true;
-                                COMport = port;
-                            } else {
-                                LogToFile.log("info",threadName + ": " + port + " wrong number");
+                            } catch (Exception e) {
+                                e.printStackTrace();
+                                LogToFile.log("info", "CONTINUING");
                             }
-                            if (reset) {
-                                completeReset();
-                            }
+                            v = true;
+                            COMport = port;
+                        } else {
+                            LogToFile.log("info", threadName + ": " + port + " wrong number");
+                        }
+                        if (reset) {
+                            completeReset();
                         }
                     }
                 } catch (Exception e) {
                     closeConnection();
                     e.printStackTrace();
-                    LogToFile.log("info","CONTINUING");
+                    LogToFile.log("info", "CONTINUING");
                     v = false;
                 }
                 closeConnection();
-                LogToFile.log("info",v + " " + COMports.size());
+                LogToFile.log("info", v + " " + COMports.size());
             }
             COMports = removeDuplicates(COMports);
         }
@@ -139,7 +136,7 @@ public abstract class GSMs extends GSM implements Runnable {
     public void run() {
         checkConnection();
         startCommands();
-        LogToFile.log("info",threadName + ": running");
+        LogToFile.log("info", threadName + ": running");
         int count = 0;
         while (!checkService()) {
             if (count > 5) {
@@ -149,22 +146,22 @@ public abstract class GSMs extends GSM implements Runnable {
                 count = 0;
             }
             count++;
-            LogToFile.log("info",threadName + ": trying to get service");
+            LogToFile.log("info", threadName + ": trying to get service");
             delay(5000);
         }
-        LogToFile.log("info",threadName + ": service");
+        LogToFile.log("info", threadName + ": service");
 
         running();
     }
 
     public boolean ownPhoneNumber() {
         String a = "";
-        for(int i = 0; i < 25; i++) {
+        for (int i = 0; i < 25; i++) {
             if (send("AT+CNUM"))
                 break;
             delay(3000);
         }
-        for(int i = 0; i < 5; i++) {
+        for (int i = 0; i < 5; i++) {
             a = jsend("AT+CNUM");
             int b = a.lastIndexOf('"') - a.indexOf('"');
             if (b > 10)
@@ -188,7 +185,7 @@ public abstract class GSMs extends GSM implements Runnable {
         try {
             TimeUnit.MILLISECONDS.sleep(s);
         } catch (InterruptedException e) {
-            LogToFile.log(e,"info", "GSMs || ");
+            LogToFile.log(e, "info", "GSMs || ");
             // TODO Auto-generated catch block
         }
     }
@@ -205,21 +202,20 @@ public abstract class GSMs extends GSM implements Runnable {
         return COMports;
     }
 
-    public void RsendMessage(String phone, String message){
+    public void RsendMessage(String phone, String message) {
         clearTransmit();
-        while(!sendMessage(phone, message))
+        while (!sendMessage(phone, message))
             clearTransmit();
-//            completeReset();
         clearTransmit();
     }
 
-    public void RmissedCall(String phone, int i){
+    public void RmissedCall(String phone, int i) {
         clearTransmit();
-        while(!missedCall(phone, i))
+        while (!missedCall(phone, i))
             clearTransmit();
-//            completeReset();
         clearTransmit();
     }
+
     public void clearTransmit() {
         jsend("" + (char) 26);
         delay(100);
@@ -229,62 +225,60 @@ public abstract class GSMs extends GSM implements Runnable {
 
     public boolean completeReset() {
         clearTransmit();
-        for(int i = 0; i < 2; i++) {
-            if (send("AT+CFUN=1,1", 20)) {
-                LogToFile.log("info", threadName + ": complete resetting: true");
-                delay(3000);
-                int count = 0;
-                while (!checkConnection()) {
-                    delay(1000);
-                    count++;
-                    if (count > 10) {
-                        clearTransmit();
-                        justReset();
-                        count = 0;
-                    }
-                }
-                startCommands();
-                count = 0;
-                while (!checkService()) {
-                    startCommands();
-                    if (count > 10) {
-                        reset();
-                        checkConnection();
-                        startCommands();
-                        count = 0;
-                    }
-                    count++;
-                    LogToFile.log("info", threadName + ": trying to get service");
-                    delay(5000);
-                }
-                LogToFile.log("info", threadName + ": service");
-                return true;
-            } else {
-                clearTransmit();
-                justReset();
+        if (send("AT+CFUN=1,1", 20)) {
+            LogToFile.log("info", threadName + ": complete resetting: true");
+            delay(3000);
+            int count = 0;
+            while (!checkConnection()) {
+                delay(1000);
+                count++;
+//                    if (count > 10) {
+//                        clearTransmit();
+//                        justReset();
+//                        count = 0;
+//                    }
             }
+            startCommands();
+            count = 0;
+            while (!checkService()) {
+                startCommands();
+                if (count > 10) {
+                    reset();
+                    checkConnection();
+                    startCommands();
+                    count = 0;
+                }
+                count++;
+                LogToFile.log("info", threadName + ": trying to get service");
+                delay(5000);
+            }
+            LogToFile.log("info", threadName + ": service");
+            return true;
+        } else {
+//                clearTransmit();
+//                justReset();
         }
-        LogToFile.log("info",threadName + ": complete resetting: false");
+        LogToFile.log("info", threadName + ": complete resetting: false");
         return false;
     }
 
     public boolean reset() {
         clearTransmit();
         if (send("AT+CFUN=1,1")) {
-            LogToFile.log("info",threadName + ": resetting: true");
+            LogToFile.log("info", threadName + ": resetting: true");
             delay(3000);
             while (!checkConnection())
                 delay(1000);
             startCommands();
             return true;
         }
-        LogToFile.log("info",threadName + ": resetting: false");
+        LogToFile.log("info", threadName + ": resetting: false");
         return false;
     }
 
-    public void justReset(){
-        for(int i = 0; i < 10; i++) {
-            serialWrite("AT+CFUN=1,1" + '\r' + '\n');;
+    public void justReset() {
+        for (int i = 0; i < 10; i++) {
+            serialWrite("AT+CFUN=1,1" + '\r' + '\n');
         }
         delay(10000);
     }
